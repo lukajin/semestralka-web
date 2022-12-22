49bad2f5611f5dc95abab210d6ebca3bcade5ec3
Jindřich Lukáš <lukaj@students.zcu.cz>
2022-12-22 18:13:13 +0100

    Testovací data - uživatelé

---
+++ dbsetup/3_data.sql
@@ -0,0 +1,7 @@
+insert into uzivatel (login, heslo, jmeno, role) values
+('tulak', '$2y$10$k7q4f2JdvbH..TjuCzumc.FRYecIuQBi59fR7oTIhYtlwRsjsRQAC', 'Pavel Tulach', 1),
+('langra', '$2y$10$bMW2tIVEqKHS0oSwQii8D.gi/X61zKHHKKvTh3PU5SuCXXaxAu0SG', 'Andrea Langrová', 1),
+('sevmich', '$2y$10$XPwDs1nElbxZ1i46FDclXehEVivMjmTGjEPte70bZ0ErA9lxwwSIu', 'Michal Ševčík', 3),
+('hanka123', '$2y$10$FNiTd/UEUbl.SGLeyIGhNOZymxMVFUM5umRDQQwl46LGAQlOrhfTy', 'Hana Tesařová', 3),
+('lipanek', '$2y$10$sCTIdZjspIqjGgI8/3aSCeF6CpQqF28tl57/KLenOQGLzyYq12ftW', 'Michaela Lipenská', 2),
+('dolajan', '$2y$10$aQj0fpKBVaPx6JQMiVZ9beUpJMyhm2ndaqqWPIzg0WsgrwndEZy/6', 'Jan Dolanský', 2);
