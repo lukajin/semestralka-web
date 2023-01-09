68e4a169251c639d1976d67f451e3756f316d406
Jindřich Lukáš <lukaj@students.zcu.cz>
2023-01-09 10:07:03 +0100

    Testovací data - oprava hesel

--- dbsetup/3_data.sql
+++ dbsetup/3_data.sql
@@ -1,7 +1,7 @@
 use web;
 
 insert into uzivatel (login, heslo, jmeno, role) values
-('root', ' $2y$10$udnduTR3pgYvot3UkOdQc.x1jVLtiYVkEuwB5USza.z4hwCeGBSHO', 'SuperAdmin', 0),
+('root', '$2y$10$udnduTR3pgYvot3UkOdQc.x1jVLtiYVkEuwB5USza.z4hwCeGBSHO', 'SuperAdmin', 0),
 ('tulak', '$2y$10$k7q4f2JdvbH..TjuCzumc.FRYecIuQBi59fR7oTIhYtlwRsjsRQAC', 'Pavel Tulach', 1),
 ('langra', '$2y$10$bMW2tIVEqKHS0oSwQii8D.gi/X61zKHHKKvTh3PU5SuCXXaxAu0SG', 'Andrea Langrová', 1),
 ('sevmich', '$2y$10$XPwDs1nElbxZ1i46FDclXehEVivMjmTGjEPte70bZ0ErA9lxwwSIu', 'Michal Ševčík', 3),
@@ -10,8 +10,8 @@ insert into uzivatel (login, heslo, jmeno, role) values
 ('dolajan', '$2y$10$aQj0fpKBVaPx6JQMiVZ9beUpJMyhm2ndaqqWPIzg0WsgrwndEZy/6', 'Jan Dolanský', 2);
 
 insert into uzivatel (login, heslo, jmeno, role, povolen) values
-('slouj', '$2y$10$/pCUAytxi/n5Jt4x2t2fA.4sTUDKr7nUE7NMRgGEbtasjCV/CbCN.', 'Jiří Slouka', 3, 'N'),
-('vycitalova', '$2y$10$FNiTd/UEUbl.SGLeyIGhNOZymxMVFUM5umRDQQwl46LGAQlOrhfTy', 'Milena Vyčítalová', 1, 'N');
+('slouj', '$2y$10$Jb0T8z0gi7fnXGs30K2IWORxL640ejzuHo3/55fcMugNOY88bAciG', 'Jiří Slouka', 3, 'N'),
+('vycitalova', '$2y$10$mYByy9sjCP4JSDwKFPNFVesw9IGJ/Dw55MqD4kzZffhSUd8pVhwku', 'Milena Vyčítalová', 1, 'N');
 
 insert into prispevek (autor, nazev, zmenen, stav, soubor, abstrakt) values
 (5,'Možnosti odstranění či redukce odpadu v moři','2022-06-10 21:25:05','A', '1_Možnosti odstranění či redukce odpadu v moři.pdf', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sagittis hendrerit ante. Etiam due, fermentum vitae, sagittis id, malesuada in, quam. Nullam at arcu a est sollicitudin euismod. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Fusce suscipit libero eget elit. Etiam posuere lacus quis dolor.'),
